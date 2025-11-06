63fb6002bdc7b84aa0fc9e77fe2ee9c27e3842e4
cynthia04-code <cynthia04@ciencias.unam.mx>
2025-11-05 21:18:03 -0600

    Create ArreglosInterface.java

---
+++ src/interfaces/arregloInterface/ArreglosInterface.java
@@ -0,0 +1,16 @@
+package interfaces.arregloInterface;
+import modulos.Empleado;
+
+public interface ArreglosInterface {
+    Empleado[] regresarReversa(Empleado[] arreglo)
+        throws IllegalArgumentException, IllegalStateException;
+
+    Empleado[] agregarElemento(Empleado[] arreglo, Empleado empleado)
+        throws IllegalArgumentException;
+
+    Empleado buscarElemento(Empleado[] arreglo, int posicion)
+        throws IllegalArgumentException, IndexOutOfBoundsException;
+
+    Empleado[] eliminarElemento(Empleado[] arreglo, int posicion)
+        throws IllegalArgumentException, IndexOutOfBoundsException;
+}
